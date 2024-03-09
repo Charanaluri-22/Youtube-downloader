410db34d83a867a6efdb145177f796273d7eceb8
charan <aluricharan1922@gmail.com>
2024-03-09 09:51:52 +0530

    updated file directory code

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 from pytube import YouTube
-from tkinter import filedialog,Tk
+from tkinter import filedialog, Tk
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
 
@@ -15,23 +16,26 @@ def download():
     try:
         yt = YouTube(video_url)
         high_stream = yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
-        print(high_stream.title)
-        high_stream.download()
-        return render_template('success.html',message=high_stream.title)
+
+        # Open file dialog to select download directory
+        root = Tk()
+        root.withdraw()  # Hide Tkinter window
+        download_dir = filedialog.askdirectory()
+        root.destroy()  # Close Tkinter window
+
+        if download_dir:
+            # Download the video to the selected directory
+            high_stream.download(download_dir)
+            return render_template('success.html', message=high_stream.title,download_dir=download_dir)
+        else:
+            return render_template('error.html', message="No download directory selected.")
+
     except Exception as e:
-        return render_template("error.html",message="Error: "+str(e))
+        return render_template("error.html", message="Error: " + str(e))
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-def open_file_dialog():
-    filepath = filedialog.askopenfilename()
-    if filepath:
-        print("Selected file:", filepath)
-    else:
-        print("No file selected")
 
 if __name__ == '__main__':
-    root = Tk()
-    root.withdraw()
     app.run(debug=True)
